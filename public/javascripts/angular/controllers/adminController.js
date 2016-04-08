fc86723e5930700da1d9c2d385c6271bbed98901
Kyle Walter <walter.kl26@gmail.com>
2016-04-08 00:59:28 -0500

    Added ability to get all music and hits and view on admin page.

--- public/javascripts/angular/controllers/adminController.js
+++ public/javascripts/angular/controllers/adminController.js
@@ -2,10 +2,30 @@
  * Created by Kyle Walter on 3/10/2016.
  */
 angular.module('weddingApp')
-    .controller('adminController',function($scope){
+    .controller('adminController',function($scope,$http){
         $scope.init = function(){
             $('body').css({
                 'background-image':'url("/images/Kyle_Katie_Engagement-53.jpg")'
             });
+            getHits();
+            getMusic();
+        }
+
+        $scope.hits = 0;
+        $scope.music = [];
+
+        var getHits = function(){
+            console.log('Hits');
+            $http.get('/api/data')
+                .success(function(data){
+                    $scope.hits = data.hits;
+                });
+        }
+
+        var getMusic = function(){
+            $http.get('/api/music')
+                .success(function(music){
+                    $scope.music = music;
+                });
         }
     });
