f2ab1935101c59064bedd0ca0bef0aeb69c98821
Kyle Walter <walter.kl26@gmail.com>
2016-04-20 00:41:56 -0500

    Fixed date on home page

--- public/javascripts/angular/controllers/homeController.js
+++ public/javascripts/angular/controllers/homeController.js
@@ -7,10 +7,10 @@ angular.module('weddingApp')
             $('body').css({
                 'background-image':'url("/images/Kyle_Katie_Engagement-78.jpg")'
             });
-            $('.countDown').countdown('2016/07/09 14:00:00',{elapse:true})
+            $('.countDown').countdown('2016/7/10 14:00:00',{elapse:true})
                 .on('update.countdown',function(event){
                     $('.month').text(event.strftime('%m'));
-                    $('.day').text(event.strftime('%d'));
+                    $('.day').text(event.strftime('%n'));
                     $('.hour').text(event.strftime('%H'));
                     $('.min').text(event.strftime('%M'));
                     $('.sec').text(event.strftime('%S'));
